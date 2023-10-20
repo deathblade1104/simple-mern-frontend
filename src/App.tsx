c58803b98bf88e994e0d4b3dc03f1d83331f8015
Shahbaz Hasan Raja <shahbaz.hasanraja@letsransport.team>
2023-10-21 03:24:15 +0530

    Temp commit

--- src/App.tsx
+++ src/App.tsx
@@ -6,22 +6,26 @@ import {
 } from 'react-router-dom';
 import NewPlaces from './modules/places/pages/NewPlaces';
 import Users from './modules/users/pages/Users';
+import MainNavigation from './shared/components/Navigation/MainNavigation/MainNavigation';
 
 function App() {
   return (
     <div>
       <Router>
-        <Switch>
-          {/* Make sure you have wrapped your app with a BrowserRouter */}
-          <Route path='/users' exact>
-            <Users />
-          </Route>
-          <Route path='/places/new' exact>
-            <NewPlaces />
-          </Route>
-          {/* This Redirect should work to redirect to the root route when no other routes match */}
-          <Redirect to='/' />
-        </Switch>
+        <MainNavigation />
+        <main>
+          <Switch>
+            {/* Make sure you have wrapped your app with a BrowserRouter */}
+            <Route path='/users' exact>
+              <Users />
+            </Route>
+            <Route path='/places/new' exact>
+              <NewPlaces />
+            </Route>
+            {/* This Redirect should work to redirect to the root route when no other routes match */}
+            <Redirect to='/' />
+          </Switch>
+        </main>
       </Router>
     </div>
   );
